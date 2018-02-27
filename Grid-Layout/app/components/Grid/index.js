9af7eedf38cca790b38fd81b215059c31fe7ee27
wstrother <wstrother@gmail.com>
2018-02-27 14:14:52 -0500

    Afternoon Lab, Grid-Layout, push to be done 2018-025-27

--- Grid-Layout/app/components/Grid/index.js
+++ Grid-Layout/app/components/Grid/index.js
@@ -10,10 +10,31 @@ import './style.css';
 import './styleM.css';
 
 export default class Grid extends React.PureComponent {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      images: [
+        "PopCorn By its Lonesome - Caramel" + ".jpg",
+        "PopCorn By its Lonesome - Cheese - Bifurcated" + ".jpg",
+        "PopCorn By its Lonesome - Cheese" + ".jpg",
+        "PopCorn By its Lonesome - Plain - Inside Look at Furls" + ".jpg",
+        "PopCorn By its Lonesome - Plain" + ".jpg"
+      ]
+    }
+  }
+
+render() {
     return (
       <div>
-
+        <div className="grid" >
+          {this.state.images.map((image, index) => (
+            <div
+              className="gridItem" key={`gridItem${index}`}
+            >
+            <img src={require('../../images/'+image)} className="gridItem"/>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
--- Grid-Layout/app/containers/Home/index.js
+++ Grid-Layout/app/containers/Home/index.js
@@ -6,16 +6,31 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import  Grid from 'components/Grid';
 
 import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
+
+constructor() {
+  super();
+  this.state = {
+    images: [
+      "PopCorn By its Lonesome - Caramel" + ".jpg",
+      "PopCorn By its Lonesome - Cheese - Bifurcated" + ".jpg",
+      "PopCorn By its Lonesome - Cheese" + ".jpg",
+      "PopCorn By its Lonesome - Plain - Inside Look at Furls" + ".jpg",
+      "PopCorn By its Lonesome - Plain" + ".jpg"
+    ]
+  }
+}
+
   render() {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
-        //Remove this line and you can start writing your code here.
+          <Grid />
       </div>
     );
   }
