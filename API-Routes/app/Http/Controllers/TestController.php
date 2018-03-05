0e406409aa178fefdd42ffea5d4d29d42da50567
Greg Slavens <gslaven@comcast.net>
2018-03-05 10:47:55 -0500

    API-Routes initial turn-in commit

---
+++ API-Routes/app/Http/Controllers/TestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Response;
+use Purifier;
+
+class TestController extends Controller
+{
+  public function index()
+  {
+    $data = "here is my data";
+    return Response::json(['data' => $data]);
+  }
+
+  public function store(Request $request)
+  {
+    $rules = [
+      'name' => 'required',
+      'age' => 'required'
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      returnResponse::json(['error' => 'Please fill in all fields.']);
+    }
+
+    $name = $request->input('name');
+    $age = $request->input('age');
+    $home = $request->input('home');
+
+    $sentence = "Hi! My name is " . $name . " and I am " . $age . " years old.  I live in " . $home . ".";
+
+    return Response::json(['sentence' => $sentence]);
+  }
+}
