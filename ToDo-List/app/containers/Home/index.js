bac23cae37d3e3f438d198cc76ad5f1b5f3a5beb
Greg Slavens <gslaven@comcast.net>
2018-03-05 15:20:38 -0500

    ToDo-FrontEnd rework on Lab7 to connect it to the Back end of Lab 6 initial turn in commit

--- ToDo-List/app/containers/Home/index.js
+++ ToDo-List/app/containers/Home/index.js
@@ -26,42 +26,82 @@ export default class Home extends React.PureComponent {
     })
   }
 
-storeItem = () => {
-  var listItems = this.state.listItems;
-  var inputItem = this.state.inputItem;
+  storeItem = () => {
+    var listItems = this.state.listItems;
+    var inputItem = this.state.inputItem;
 
-if(inputItem!== "") {
-  listItems.push(inputItem);
-  this.setState({
-    listItems:listItems,
-    inputItem:""
+    if(inputItem!== "") {
+      listItems.push(inputItem);
+      this.setState({
+        listItems:listItems,
+        inputItem:""
+      })
+    }
+  }
+
+  getTasks = () => {
+    fetch("http://127.0.0.1:8000/api/getTasks", {
+      method:'GET'
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json){
+      this.setState({
+        listItems:json.tasks
+      })
+    }.bind(this))
+  };
+
+storeTask = () => {
+  let data = new FormData();
+  data.append('taskContent', this.state.inputItem);
+
+  fetch("http://127.0.0.1:8000/api/storeTask", {
+    method:'POST',
+    body:data})
+  .then(function(response) {
+    return response.json();
   })
+  .then(function(json) {
+    let listItems = this.state.listItems;
+    listItems.push(json.task);
+    this.setState({
+      listItems:listItems
+    })
+    this.forceUpdate();
+  }.bind(this))
 }
+
+componentWillMount(){
+this.getTasks();
 }
 
   render() {
     return (
       <div className="container">
         <Helmet title="Lab 4 To-Do List" meta={[ { name: "description",
-        content: "Lab4 of the Clubhou.se's Code Boot Camp 3" }]}/>
-        <div className="inputContainer">
-          <input type="text"
-            className="todoInput"
-            onChange={this.handleItem}
-            value={this.state.inputItem} />
-          <input type="submit"
-            className="todoButton"
-            onClick={this.storeItem}
-          value="Add this ToDo, to your list."/>
-        </div>
-        <div className="todoList">
-          {this.state.listItems.map((item, index) => (
-            <div className="listItem" key={`listItem${index}`}>
-            {item}
+          content: "Lab4 of the Clubhou.se's Code Boot Camp 3" }]}/>
+          <div className="inputContainer">
+            <input type="text"
+              className="todoInput"
+              onChange={this.handleItem}
+              value={this.state.inputItem} />
+              <input type="submit"
+                className="todoButton"
+                onClick={this.storeTask}
+                value="Add this ToDo, to your list."/>
+              </div>
+              <div className="todoList">
+                {this.state.listItems.map((item, index) => (
+                  <div className="listItem"
+                    key={`listItem${index}`}
+                    onClick={this.strikeThrough}>
+                    {item.taskContent}
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+          );
+        }
+      }
