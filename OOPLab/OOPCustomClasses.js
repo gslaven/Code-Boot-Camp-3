ada09251047ae8a75dfa34eb45e709003d6d979b
Greg <gslaven@comcast.net>
2018-03-09 06:54:30 -0500

    Got the UL HTML element sorta working, need to revisit for the fnCalcWeight display

--- OOPLab/OOPCustomClasses.js
+++ OOPLab/OOPCustomClasses.js
@@ -66,78 +66,134 @@ class Club
   }
   get Weight()
   {
-    //returns the calculated weight given by the fnCalcWeight function
-    return fnCalcWeight();
+    //returns the calculated weight given by the function fnCalcWeight
+    return fnCalcWeight(this.Shaft, this.Length);
+  }
+}
+/*Calculates weight of the club using length and shaft material lbs/in**3 estimates from here: http://www.psyclops.com/tools/technotes/materials/density.html &
+clube head estimates https://www.calculator.net/conversion-calculator.html
+*/
+function fnCalcWeight(paramShaft, paramLength)
+{
+  let varResults = 0;
+  
+  // What material is this club made out of
+  switch (String(paramShaft).toLowerCase())
+  {
+    case String("Steel").toLowerCase():
+      //If Steel, use Density of 0.28 lb/in**3(cubed) + Estimated weight of Club Head
+      varResults = (paramLength * 0.28) + 0.440924884; //gram2lbs from web
+      break;
+    case String("Graphite").toLowerCase():
+      //If Graphite, use Density of 0.08 lb/in**3(cubed)
+      varResults = (paramLength * 0.08) + 0.440924884; //gram2lbs from web
+      break;
+    default:
+      //If neither, then just return 0
+      varResults = 0;
+    break;
+  }
+  return varResults;
+}
+
+//Concrete Class - Driver
+class Driver extends Club // extends = <----- Inheritance ----->
+{
+  Swing(paramClub) 
+  {
+    return "Place ball just inside front foot of your stance.  Start backswing and rotate as far as you comfortablly can.  Start downswing and let 'er rip!!"
+
   }
 
-  /*Calculates weight of the club using length and shaft material lbs/in**3 estimates from here: http://www.psyclops.com/tools/technotes/materials/density.html
-  */
-  fnCalcWeight()
-  {
-   let varResults = 0;
-   // What material is this club made out of
-   switch (Shaft.ToLower())
-   {
-     case String("Steel").ToLower:
-       //If Steel, use Density of 0.28 lb/in**3(cubed)
-       varResults = (Length * 0.28);
-       break;
-     case String("Graphite").ToLower:
-       //If Graphite, use Density of 0.08 lb/in**3(cubed)
-       varResults = (Length * 0.08);
-       break;
-     default:
-       //If neither, then just return 0
-       varResults = 0;
-     break;
-   }
-   return varResults; 
- }
 }
 
-
-class Driver extends Club 
+class NineIron extends Club // extends = <----- Inheritance ----->
 {
+  constructor(length, shaft, grip, loft, chipping)
+  {
+    super(length, shaft, grip, loft);
+    
+
+  }
+
+  Swing(paramClub) 
+  {
+    return "Place ball just inside front foot of your stance.  Start backswing and rotate as far as you comfortablly can.  Start downswing and let 'er rip!!"
+
+  }
 
 }
+
+//get the output div
+let outputDiv = document.getElementById("output");
+
+//Create a HTML Unstructured List (ul) element to store the tests and results for display
+let varResultsUl = document.createElement("ul");
 
 function Test ()
 {
-  //get the output div
-  let outputDiv = document.getElementById("output");
-  //Clear out any potential data in the div
-  outputDiv.innerHTML = "";
-
-  //Create a HTML Unstructured List (ul) element to store the tests and results for display
-  let varResultsUl = document.createElement("ul");
-  outputDiv.appendChild(varResultsUl)
-
-  //Create a new List Item (li) to add to the UL
-  let varResultsLI = document.createElement("li");
-
-  //Create a HTML Text Node to store the tests and results for display
-  let varResultsNode = document.createTextNode("");
+  //Test description to be displayed in HTML
+  fnDisplayTestName("Raw Club Instantiation, default or no values");
 
   //Test the club instantiation and set the variables
   let varClub = new Club(-1, "testClubShaft", "testClubGrip", -1 );
+  fnDisplayLoopResults(varClub);
+
+  //Test description to be displayed in HTML
+  fnDisplayTestName("Driver Instantiation, random settings but different from default club");
 
   //test the club instantiation by changing values
   varClub.Length = 48;
   varClub.Shaft = "Steel";
   varClub.Grip = "Foam";
   varClub.Loft = 12;
+  fnDisplayLoopResults(varClub);
+}
+
+function fnDisplayTestName(paramTestName) 
+{
+  //Create a new List Item (li) to add to the UL
+  let varResultsLI = document.createElement("li");
+
+  //Create a HTML Text Node to store the tests description for display
+  let varResultsNode = document.createTextNode(paramTestName);
+
+  //Append the list item
+  varResultsLI.appendChild(varResultsNode);
+
+  //Take results of the for loop and display them in a list
+  varResultsUl.appendChild(varResultsLI);
+
+  //Displays the results of the js setting variables and Classes
+  outputDiv.appendChild(varResultsUl);
+}
+
+function fnDisplayLoopResults(paramClub) 
+{
+  //Create a new List Item (li) to add to the UL
+  let varResultsLI = document.createElement("li");
+
+  //Create a HTML Text Node to display the tests results
+  let varResultsNode = document.createTextNode("");
 
   //Build HTML by looping through properties
-  for (var prop in varClub)
+  for (var prop in paramClub)
   {
-  //Create a HTML Text Node to store the tests and results for display
-  varResultsNode = document.createTextNode("(" +prop + " = " + varClub[prop] + ") // ");
-  varResultsLI.appendChild(varResultsNode);
+    //Create a HTML Text Node to store the tests and results for display
+    varResultsNode = document.createTextNode("(" +prop + " = " + paramClub[prop] + ") // ");
+    varResultsLI.appendChild(varResultsNode);
   }
+
+  //Test description to be displayed in HTML
+  fnDisplayTestName("Club weightInstantiation, default or no values");
+    
+  //Create a HTML Text Node to display the function fnGetWeight results
+  varResultsNode = document.createTextNode(paramClub.Weight);
+  varResultsLI.appendChild(varResultsNode);
+
   //Take results of the for loop and display them in a list
   varResultsUl.appendChild(varResultsLI);
   //Displays the results of the js setting variables and Classes
   outputDiv.appendChild(varResultsUl);
 }
-Test();
 Test();
