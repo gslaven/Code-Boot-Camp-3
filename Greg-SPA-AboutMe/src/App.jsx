3739f89d2000b0d652eb210eba1b80705dc86226
GAS <32984362+gslaven@users.noreply.github.com>
2018-03-21 00:45:41 -0400

    Got the routes to sorta work.  Need to figure out how to display just one at the time, maybe a switch or an index route on the home page???

--- Greg-SPA-AboutMe/src/App.jsx
+++ Greg-SPA-AboutMe/src/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Switch } from 'react-router'
+import { Transition, TransitionGroup } from 'react-transition-group'
 
 import './App.css';
 
 import logo from './logo.svg';
 import NavBar from './NavBar.jsx';
 
+const styles = {
+    animatedElement: {
+        position: 'fixed'
+    }
+}
 const Home = () => (
     <div>
         <h1 id="gregsH1">Greg Slavens ... some background</h1>
-        <blockquote id="blqMyInfo" class="quotez">
+        <blockquote id="blqMyInfo" className="quotez">
             "I was born a poor bla... wait a minute, I think that"s
             from the movie "The Jerk"!?
-          <br /> <br />
+            <br /> <br />
             OK, OK now for the real deal.  I was born, raised
             and most likely, will die in Augusta, GA.  I attended
-            Westside High School, Augusta College (yes, it"s still
-            called that in my world), and the Clubhou.se"s Code Boot Camp.
-          <br /> <br />
+            Westside High School, Augusta College (yes, it"s still called that in my world), and the Clubhou.se"s Code Boot Camp.
+            <br /> <br />
             I have worked for 4 companies in my career, so job hopping
             is not an issue for me.
-          <br /> <br />
+            <br /> <br />
             The first was the Medical College of Georgia (yes, it"s still
-                called that in my world), CallingPost.com, SRNS,
-                and Macuch Steel.
-        </blockquote>
+            called that in my world), CallingPost.com, SRNS,
+            and Macuch Steel.
+            </blockquote>
     </div >
 );
 
@@ -36,46 +40,55 @@ const Contact = () => (
     </div >
 );
 
-const App = () => (
-    <div id="root" className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <NavBar />
-        </header>
-        <Fade>
+const App = () => {
+    return (
+        <div id="root" className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <NavBar />
+            </header>
             <Router>
                 <div>
-                    <Switch>
-                        <Route path="/Home" component={Home} />
-                        <Route path="/Contact" component={Contact} />
-                        <Route component={Home} />
-                    </Switch>
+                    <TransitionGroup>
+                        <Transition
+                            timeout={1000}
+                            onEnter={el => console.log('enter', el)}
+                            onExit={el => console.log('exit', el)}
+                        >
+                            <section style={styles.animatedElement}>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/Home" component={Home} />
+                                <Route exact path="/Contact" component={Contact} />
+                                <Route component={Home} />
+                            </section>
+                        </Transition>
+                    </TransitionGroup>
                 </div>
             </Router>
-        </Fade>
-        <p className="App-intro">
-        </p>
-    </div>
-);
+            <p className="App-intro">
+            </p >
+        </div >
+    )
+}
 
 export default App;
 
 
 /*
-  <Route path='/' component={App}>
-     <IndexRoute exact path="/home" component={Home} />
-     <Route path='contact' component={Contact} />
-     <Route path='*' component={Home} />
- </Route>
+<Route path='/' component={App}>
+<IndexRoute exact path="/home" component={Home} />
+<Route path='contact' component={Contact} />
+<Route path='*' component={Home} />
+</Route>
 
-import { Switch, Route } from 'react-router'
+import {Switch, Route } from 'react-router'
 
 <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/:user" component={User} />
-    <Route component={NoMatch} />
-</Switch> 
+<Route exact path="/" component={Home} />
+<Route path="/about" component={About} />
+<Route path="/:user" component={User} />
+<Route component={NoMatch} />
+</Switch>
 
 */
 
