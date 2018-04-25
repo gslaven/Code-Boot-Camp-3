6d865b690553107e720b3007403927987c52f202
Greg <32984362+gslaven@users.noreply.github.com>
2018-04-25 15:44:40 -0400

    NFL website trial

--- NFL_XML_Requests/NFL.js
+++ NFL_XML_Requests/NFL.js
@@ -24,78 +24,121 @@ let xhr = new XMLHttpRequest();
 //Using Heroku to allow for CORS issues to not catch up with me
 try {
     console.log("In Try");
+    console.log("xhr.open");
     xhr.open('GET', proxyURL + requestURL, true);
+    console.log("xhr.send");
     xhr.send();
+    console.log("xhr.addEventListener");
     xhr.addEventListener("readystatechange", processRequest, false);
-    console.log(xhr);
+    console.log("End of Try");
 } catch (error) {
     console.log("In Catch");
     let varErr = "The following error happened: <br>";
-    varErr = varErr + er.description;
-    document.getElementById('h1Display').innerHTML = varErr;
+    varErr = varErr + error.description;
+    console.log("varErr:" + varErr);
+    createNewDiv(varErr, 0);
+    console.log("Just Created a newDiv");
 }
 
 function processRequest(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        let response = JSON.parse(xhr.responseText);
-        console.log("Response Object:");
-        console.log("xhr.readyState: " + xhr.readyState);
-        console.log("xhr.status: " + xhr.status);
-        console.log("xhr.responseText: " + xhr.responseText);
-        console.log("Response Object");
-        console.log(response[0]);
-        console.log(response);
-        response.sort(function(a, b){
-            return a.fullName.split(" ")[1].localeCompare(b.fullName.split(" ")[1]);
-        });
-        if (response.length === 0) {
-            console.log("Error found in response object");
-            // response.current_observation.temp_c);
-            displayError();
-        } else {
-            console.log("No Error");
-            // response.current_observation.temp_c);
-            displayResults(response);
+    console.log("In processRequest");
+    console.log("xhr.readyState:" + xhr.readyState);
+    console.log("xhr.status:" + xhr.status);
+    console.log("xhr.responseText:" + xhr.responseText);
+    console.log(xhr);
+    try {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log("In processReuest: If statement");
+            let response = JSON.parse(xhr.responseText);
+            console.log("response.length:  " + response.length);
+            console.log(response);
+            response.sort(function (a, b) {
+                return a.fullName.split(" ")[1].localeCompare(b.fullName.split(" ")[1]);
+            });
+            if (xhr.responseText.length === 0) {
+                console.log("Error found in response object");
+                console.log("displayError()");
+                displayError();
+            } else {
+                let varDispText = [];
+                let counter = 0;
+                for (elm in paramResponse) {
+                    console.log(elm);
+                    varDispText = varDispText +
+                        paramResponse[elm]["fullName"] + " (Jersey # - " +
+                        paramResponse[elm]["uniformNumber"] + ")<br>" +
+                        `<a href="` +
+                        paramResponse[elm]["profileUrl"] +
+                        `">Player Profile on NFL.com</a><br>`;
+                    counter++;
+                    console.log(varDispText);
+                    console.log(counter);
+                    displayResults(varDispText, counter);
+                }
+            }
         }
-    }
-    else {
-        displayMajorError(xhr)
+    } catch (error) {
+        console.log("In processRequest Catch");
+        let varErr = "The following error happened: <br>";
+        varErr = varErr + error.description;
+        console.log("varErr:" + varErr);
+        displayMajorError(varErr, 0);
+        console.log("Just Created a newDiv");
     }
 }
 
-function displayResults(paramResponse) {
-    let varDispText = [];
+function displayResults(paramDispTest) {
     console.log("In displayResults()");
-    for (elm in paramResponse) {
-        console.log(elm);
-        varDispText = varDispText +
-            paramResponse[elm]["fullName"] + " (Jersey # - " +
-            paramResponse[elm]["uniformNumber"] + ")<br>" +
-            `<a href="` +
-            paramResponse[elm]["profileUrl"] +
-            `">Player Profile on NFL.com</a><br>`;
-    }
-    console.log(varDispText);
-    document.getElementById('h1Display').innerHTML = varDispText;
+    createNewDiv(paramDispTest, counter);
+    console.log("Just Created a displayResults div");
 }
 
-function displayError() {
+function displayError(paramErrorText = "") {
     console.log("In displayError()");
-    document.getElementById('h1Display').innerHTML =
-        "Sorry, something has gone wrong with your request at :<br><br>'"
-        + proxyURL + "<br>" + requestURL + "'. <br><br>"
-        + "Please check the URL and the spelling in it!<br>"
-        + "Most likely this is caused by a spelling error "
-        + "for the team. <br><br>"
-        + "Please try again!<br>";
+    if (paramErrorText.length === 0) {
+        paramErrorText =
+            "Sorry, something has gone wrong with your request at :<br><br>'" +
+            proxyURL + "<br>" + requestURL + "'. <br><br>" +
+            "Please check the URL and the spelling in it!<br>" +
+            "Most likely this is caused by a spelling error " +
+            "for the team. <br><br>" +
+            "Please try again!<br>";
+    }
+    createNewDiv(paramErrorText, 0);
 }
 
-function displayMajorError(paramErrResponse) {
-    let varErrText = [];
+function displayMajorError(paramXHRError) {
     console.log("In displayMajorError()");
-    console.log(paramErrResponse);
-    for (elm in paramErrResponse) {
-        varErrText = varErrText + paramErrResponse[elm] + "<br>";
-    };
-    document.getElementById('h1Display').innerHTML = varErrText;
+    console.log("paramXHRError:");
+    console.log(paramXHRError);
+    if (paramXHRError.length === 0) {
+        paramXHRError =
+            "Sorry, something has gone wrong with your request at :<br><br>'" +
+            proxyURL + "<br>" + requestURL + "'. <br><br>" +
+            "Please check the URL and the spelling in it!<br>" +
+            "Most likely this is caused by a spelling error " +
+            "for the team. <br><br>" +
+            "Please try again!<br>";
+    }
+    console.log("paramXHRError: " + paramXHRError);
+    createNewDiv(paramXHRError, 0);
+}
+
+function createNewDiv(paramDispText, paramSequenceNum = 0) {
+    console.log("In createNewDiv");
+    console.log("paramDispText:" + paramDispText);
+    let newDiv = document.createElement('div');
+    newDiv.id = "atlDiv" + paramSequenceNum;
+    console.log(paramSequenceNum % 2);
+    if (paramSequenceNum % 2 === 0) {
+        //This is an even row so give the even class
+        newDiv.className = "even"
+    } else {
+        //This is an odd row so give the odd class
+        newDiv.className = "odd"
+    }
+    console.log(newDiv);
+    console.log(paramDispText);
+    newDiv.innerHTML = paramDispText;
+    document.body.appendChild(newDiv);
 }
