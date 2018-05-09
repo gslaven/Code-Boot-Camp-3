31aecec574b481f0d05037082cae6f307b622e9b
GAS <32984362+gslaven@users.noreply.github.com>
2018-05-09 01:01:02 -0400

    Got to tutorial #4 of DJango tutorials
    Need to get polls/index working

--- GregSlavens-DjangoTutorial/mysite/polls/urls.py
+++ GregSlavens-DjangoTutorial/mysite/polls/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
 from . import views
 
+
+app_name = 'polls'
+
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    # added the word 'specifics'
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
