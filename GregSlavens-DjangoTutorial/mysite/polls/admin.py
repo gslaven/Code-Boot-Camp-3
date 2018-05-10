7f6cb31f630dbb923c34707502d67c8817ac5ef8
Greg <32984362+gslaven@users.noreply.github.com>
2018-05-10 15:48:52 -0400

    started tutorial 7 (https://docs.djangoproject.com/en/2.0/intro/tutorial07/)

--- GregSlavens-DjangoTutorial/mysite/polls/admin.py
+++ GregSlavens-DjangoTutorial/mysite/polls/admin.py
@@ -3,5 +3,28 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+#Thois will produce a list of new choices
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+
+#This produces a tabular view of the new choices to
+# conserve screen real-etate
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
