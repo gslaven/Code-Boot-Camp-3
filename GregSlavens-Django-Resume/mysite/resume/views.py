d8efa3eba6060128c36fab5d88c0eae89b529478
GAS <32984362+gslaven@users.noreply.github.com>
2018-05-16 23:51:04 -0400

    https://docs.djangoproject.com/en/2.0/intro/tutorial03/ - Have access to all tables at once...might be powerful.

--- GregSlavens-Django-Resume/mysite/resume/views.py
+++ GregSlavens-Django-Resume/mysite/resume/views.py
@@ -1,10 +1,29 @@
 from django.http import HttpResponse
-# Create your views here.
+from .models import Address
+from .models import Company
+from .models import Job
+from .models import Phone
+from .models import Reference
+from .models import School
+from .models import Skill
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the %s index page." % request.get_full_path())
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the %s index page." % request.get_full_path())
+    latest = Company.objects.order_by('-created_at')[:5]
+    output = ''.join(var.name for var in latest)
+    return HttpResponse(output + output)
+
+
+# def index(request):
+#     latest_addresses = Address.objects.order_by('-created_at')[:5]
+#     output = ', '.join([
+#         add.street + ', ' + add.city + ',' + add.state
+#         for add in latest_addresses])
+#     return HttpResponse(output)
+
 
 def detail(request, param_id):
     return HttpResponse("Detail of %s as requested by  %s" % (param_id, request.get_full_path()))
-
