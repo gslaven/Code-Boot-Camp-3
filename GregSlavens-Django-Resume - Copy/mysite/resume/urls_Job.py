5c0398d87e9b7878b502fad9ac935e0e088b364e
Greg <32984362+gslaven@users.noreply.github.com>
2018-05-21 15:50:43 -0400

    Added all of my skills. Need to add the last of my jobs. and pretty this up.

---
+++ GregSlavens-Django-Resume - Copy/mysite/resume/urls_Job.py
@@ -0,0 +1,27 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path, re_path
+
+from . import views
+
+urlpatterns = [
+    # ex: /Resume/
+    path('', views.index, name='index'),
+    path('<int:param_id>/', views.detail, name='detail'),
+    # Singlets of each type (ie: company 1)
+    re_path(r'^(?i)address/<int:param_id>/', views.detail, name='detail'),
+    re_path(r'^(?i)phone/<int:param_id>/detail', views.detail, name='detail'),
+]
